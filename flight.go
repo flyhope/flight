38f93be0b89377ed874320d564ebcd3929e508d7
flyhope <i@chengxuan.li>
2021-07-07 08:13:59 +0800

    use channel for Broadcast

--- flight.go
+++ flight.go
@@ -5,25 +5,29 @@ import (
 	"sync/atomic"
 )
 
+type result struct {
+	val interface{}
+	err error
+}
+
 type call struct {
-	num  uint32
-	val  interface{}
-	err  error
-	cond sync.Cond
+	num     uint32
+	waitNum uint32
+	channel chan *result
 }
 
 type Group struct {
 	m sync.Map
 }
 
-func (g *Group) Do (max uint32, key string, callback func() (interface{}, error)) (interface{}, error)  {
+func (g *Group) Do(max uint32, key string, callback func() (interface{}, error)) (interface{}, error) {
 	// load map value
 	var c *call
 	if storeCall, ok := g.m.Load(key); ok {
 		c = storeCall.(*call)
 	} else {
 		// set map value
-		c = &call{}
+		c = &call{channel: make(chan *result)}
 		if storeCall, ok := g.m.LoadOrStore(key, c); ok {
 			c = storeCall.(*call)
 		}
@@ -32,17 +36,24 @@ func (g *Group) Do (max uint32, key string, callback func() (interface{}, error)
 	// wait value
 	num := atomic.AddUint32(&c.num, 1)
 	if num > max {
-		c.cond.Wait()
-		return c.val, c.err
+		atomic.AddUint32(&c.waitNum, 1)
+		if ret, ok := <-c.channel; ok {
+			return ret.val, ret.err
+		}
+		return callback()
 	}
 
 	val, err := callback()
-	c.val = val
-	c.err = err
 	g.m.Delete(key)
 
-	// todo map after Broadcast for wait
-	c.cond.Broadcast()
+	ret := &result{val: val, err: err}
+	for i := uint32(0); i < c.waitNum; i++ {
+		select {
+		case c.channel <- ret:
+		default:
+		}
+	}
+	close(c.channel)
 
 	return val, err
 }
